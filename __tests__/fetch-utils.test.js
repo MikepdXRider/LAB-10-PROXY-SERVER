7c3c44864048b627e8197046f908918ebd1f4659
Michael Rider <mikepdxrider@gmail.com>
2021-10-08 22:58:33 -0700

    adds trails endpoint, munge data function, and tests

--- __tests__/fetch-utils.test.js
+++ __tests__/fetch-utils.test.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const { mungeReviewData, mungeLocationData, mungeWeatherData } = require('../lib/fetch-utils.js');
-const { testLocationData, testReviewData, testWeatherData } = require('../data/test-data.js');
+const { mungeReviewData, mungeLocationData, mungeWeatherData, mungeTrailData } = require('../lib/fetch-utils.js');
+const { testLocationData, testReviewData, testWeatherData, testTrailData } = require('../data/test-data.js');
 
 describe('app routes', () => {
   describe('routes', () => {
@@ -32,7 +32,7 @@ describe('app routes', () => {
     });
 
 
-    test('mungeWeatherData get returns expected data', async() => {
+    test('mungeReviewData get returns expected data', async() => {
   
       const actual = mungeReviewData(testReviewData);
   
@@ -42,6 +42,27 @@ describe('app routes', () => {
         price: expect.any(String), 
         rating: expect.any(Number), 
         url: expect.any(String) 
+      };
+  
+      expect(actual).toEqual(expect.arrayContaining([expectation]));
+    });
+
+
+    test('mungeTrailData get returns expected data', async() => {
+  
+      const actual = mungeTrailData(testTrailData);
+  
+      const expectation = {
+        name: expect.any(String),
+        location: expect.any(String),
+        length: expect.any(String),
+        stars: expect.any(String),
+        star_votes: expect.any(String),
+        summary: expect.any(String),
+        trail_url: expect.any(String),
+        conditions: expect.any(String),
+        condition_date: expect.any(String),
+        condition_time: expect.any(String)
       };
   
       expect(actual).toEqual(expect.arrayContaining([expectation]));
